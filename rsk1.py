10caf76ee3649778252ee1cc274dc728886ccaac
EC2 Default User <ec2-user@ip-172-31-2-150.ap-south-2.compute.internal>
2026-04-03 14:54:59 +0000

    New Feature

--- rsk1.py
+++ rsk1.py
@@ -2,3 +2,6 @@ print("Hello World")
 print("Welcome to RSK1")
 print("This is a simple Python script.")
 print("You can run this script to see the output.")
+'''New script'''
+print("New Feature")
+print("New script")
